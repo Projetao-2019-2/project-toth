1ea3412a987979d868e12b736571b0f4500bf6c6
Zambom <erikzambom@hotmail.com>
2019-10-18 12:08:14 -0300

    Documenting post endpoints

--- src/controllers/v1/post.js
+++ src/controllers/v1/post.js
@@ -1,6 +1,40 @@
 const { Post, sequelize } = require('../../models')
 
 class PostController {
+  /**
+   * @swagger
+   * /posts:
+   *  get:
+   *    tags:
+   *      - Posts
+   *    description: Returns a list with all posts
+   *    produces:
+   *      - application/json
+   *    responses:
+   *      200:
+   *        description: Successfully retrives the list of posts
+   *        schema:
+   *          $ref: '#/components/schemas/ExtendedPostModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                posts:
+   *                  type: array
+   *                  items:
+   *                    type: object
+   *                    $ref: '#/components/schemas/ExtendedPostModel'
+   *      500:
+   *        description: The server was unable to get the list of posts
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async list (req, res) {
     const posts = await Post.findAll({ include: ['files'] })
 
@@ -11,6 +45,44 @@ class PostController {
     res.json({ posts })
   }
 
+  /**
+   * @swagger
+   * /posts/{id}:
+   *  get:
+   *    tags:
+   *      - Posts
+   *    description: Returns a specific post queried by id
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    responses:
+   *      200:
+   *        description: Successfully retrieves the post queried
+   *        schema:
+   *          $ref: '#/components/schemas/ExtendedPostModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                post:
+   *                  type: object
+   *                  $ref: '#/components/schemas/ExtendedPostModel'
+   *      404:
+   *        description: The server was unable to find the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async view (req, res) {
     const { id } = req.params
     const post = await Post.findOne({ where: { id }, include: ['files'] })
@@ -22,6 +94,60 @@ class PostController {
     res.json({ post })
   }
 
+  /**
+   * @swagger
+   * /posts:
+   *  post:
+   *    tags:
+   *      - Posts
+   *    description: Creates a post instance
+   *    produces:
+   *      - application/json
+   *    requestBody:
+   *      content:
+   *        multipart/form-data:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              texto:
+   *                type: string
+   *              file:
+   *                type: array
+   *                items:
+   *                  type: string
+   *                  format: binary
+   *    responses:
+   *      201:
+   *        description: Successfully creates a post
+   *        schema:
+   *          $ref: '#/components/schemas/ExtendedPostModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                post:
+   *                  type: object
+   *                  $ref: '#/components/schemas/ExtendedPostModel'
+   *      400:
+   *        description: Attempt to create an empty post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      500:
+   *        description: The sever was unable to create the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async create (req, res) {
     const { files } = req
     const { texto } = req.body
@@ -52,6 +178,80 @@ class PostController {
     res.status(201).json({ post })
   }
 
+  /**
+   * @swagger
+   * /posts/{id}:
+   *  put:
+   *    tags:
+   *      - Posts
+   *    description: Updates a post instance
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    requestBody:
+   *      content:
+   *        multipart/form-data:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              texto:
+   *                type: string
+   *              fileids:
+   *                type: array
+   *                description: The ids of the already existing files in the post
+   *                items:
+   *                  type: integer
+   *              file:
+   *                type: array
+   *                items:
+   *                  type: string
+   *                  format: binary
+   *    responses:
+   *      200:
+   *        description: Successfully updates a post
+   *        schema:
+   *          $ref: '#/components/schemas/ExtendedPostModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                post:
+   *                  type: object
+   *                  $ref: '#/components/schemas/ExtendedPostModel'
+   *      400:
+   *        description: Attempt to make an empty post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      404:
+   *        description: The server was unable to find the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      500:
+   *        description: The sever was unable to update the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async update (req, res) {
     const { id } = req.params
     const { files } = req
@@ -110,6 +310,50 @@ class PostController {
     res.json({ post: p })
   }
 
+  /**
+   * @swagger
+   * /posts/{id}:
+   *  delete:
+   *    tags:
+   *      - Posts
+   *    description: Deletes a post instance
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    responses:
+   *      200:
+   *        description: Successfully deletes the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      404:
+   *        description: The server was unable to find the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      500:
+   *        description: The sever was unable to delete the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async delete (req, res) {
     const { id } = req.params
 
@@ -128,6 +372,55 @@ class PostController {
     res.json({ message: 'Post deleted successfully!' })
   }
 
+  /**
+   * @swagger
+   * /posts/{id}:
+   *  patch:
+   *    tags:
+   *      - Posts
+   *    description: Evaluates a post instance in useful or not
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    requestBody:
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              increments:
+   *                type: string
+   *                enum:
+   *                  - util
+   *                  - n_util
+   *    responses:
+   *      200:
+   *        description: Successfully evaluates a post
+   *        schema:
+   *          $ref: '#/components/schemas/BasicPostModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                post:
+   *                  type: object
+   *                  $ref: '#/components/schemas/BasicPostModel'
+   *      404:
+   *        description: The server was unable to find the post
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async evaluate (req, res) {
     const { id } = req.params
     const { increment } = req.body

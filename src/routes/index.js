7cbca1a503ea92029708cb9351634e18850050ba
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2019-10-18 22:16:05 -0300

    Add User model CRUD routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,51 @@ router.get('/', (req, res, next) => {
   res.status(418).json({ title: '🍵' })
 })
 
+router.get('/users', (req, res, next) => {
+  User.findAll().then( users => {
+    res.send(users);
+  });
+
+});
+
+router.post('/users/new', (req, res, next) => {
+  User.create({
+    nome: req.body.nome,
+    email: req.body.email,
+    curso: req.body.curso,
+    ies: req.body.ies,
+    senha: req.body.senha
+  }).then( user => {
+    res.send('Usuário criado');
+  })
+});
+
+router.get('/users/:id', (req, res, next) => {
+  let user_id = req.params.id
+
+  User.findOne({ where: {id: user_id} }).then( user => {
+    res.json(user);
+  })
+});
+
+router.put('/users/:id', (req, res, next) => {
+  let user_id = req.params.id
+
+  User.findOne({ where: { id: user_id }}).then( user => {
+    user.update(req.body).then( updated_user => {
+      res.json(updated_user);
+    });
+  });
+});
+
+router.delete('/users/:id', (req, res, next) => {
+  let user_id = req.params.id
+
+  User.destroy({ where: {id: user_id} }).then( () => {
+    res.send("Usuário excluído")
+  });
+});
+
 router.use(`/${process.env.API_VERSION}/posts`, postRouter)
 
 router.use(`/${process.env.API_VERSION}/swagger.json`, (req, res) => {

140b39d79ea3f4abaddd156c4ecf0e7c57d4434f
lgsa2 <56773679+lgsa2@users.noreply.github.com>
2019-10-20 14:45:20 -0300

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,5 @@
 const PostController = require(`./${process.env.API_VERSION}/post`)
+const QuestionController = require(`./${process.env.API_VERSION}/comment`)
 
 module.exports = { PostController }
+module.exports = { PostController, QuestionController }

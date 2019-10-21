a2a6c31707b9dc35daf5b58c36fa7bb7bd4aab5e
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2019-10-21 09:49:48 -0300

    Fix routes/index.js edited incorrectly

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,28 @@
 const express = require('express')
+const router = express.Router()
+
+const swaggerUI = require('swagger-ui-express')
+const { swaggerConfig: swagger } = require('../config/swagger')
 
 const postRouter = require(`./${process.env.API_VERSION}/postRouter`)
 const userRouter = require(`./${process.env.API_VERSION}/userRouter`)
+
+router.get('/', (req, res, next) => {
+  res.status(418).json({ title: '🍵' })
+})
+
+router.use(`/${process.env.API_VERSION}/posts`, postRouter)
+router.use(`/${process.env.API_VERSION}/users`, userRouter)
+
+router.use(`/${process.env.API_VERSION}/swagger.json`, (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(swagger)
+})
+
+router.use(
+  `/${process.env.API_VERSION}/docs`,
+  swaggerUI.serve,
+  swaggerUI.setup(swagger)
+)
+
+module.exports = router

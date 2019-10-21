6cbc08c8471bf81b379a368aa8e4fe8550052650
Zambom <erikzambom@hotmail.com>
2019-10-21 17:24:55 -0300

    Documenting question endpoints

--- src/controllers/v1/question.js
+++ src/controllers/v1/question.js
@@ -1,6 +1,46 @@
 const { Question } = require('../../models')
 
 class QuestionController {
+  /**
+   * @swagger
+   * /questions:
+   *  post:
+   *    tags:
+   *      - Questions
+   *    description: Creates a question instance
+   *    produces:
+   *      - application/json
+   *    requestBody:
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              text:
+   *                type: string
+   *    responses:
+   *      201:
+   *        description: Successfully creates a question
+   *        schema:
+   *          $ref: '#/components/schemas/BasicQuestionModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                question:
+   *                  type: object
+   *                  $ref: '#/components/schemas/BasicQuestionModel'
+   *      500:
+   *        description: The sever was unable to create the question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async create (req, res) {
     const question = await Question.create({
       text: req.body.text
@@ -13,6 +53,44 @@ class QuestionController {
     res.json({ question })
   }
 
+  /**
+   * @swagger
+   * /questions/{id}:
+   *  get:
+   *    tags:
+   *      - Questions
+   *    description: Returns a specific question queried by id
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    responses:
+   *      200:
+   *        description: Successfully retrieves the question queried
+   *        schema:
+   *          $ref: '#/components/schemas/BasicQuestionModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                question:
+   *                  type: object
+   *                  $ref: '#/components/schemas/BasicQuestionModel'
+   *      404:
+   *        description: The server was unable to find the question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async view (req, res) {
     const { id } = req.params
     const question = await Question.findOne({ where: { id } })
@@ -24,6 +102,40 @@ class QuestionController {
     res.json({ question })
   }
 
+  /**
+   * @swagger
+   * /questions:
+   *  get:
+   *    tags:
+   *      - Questions
+   *    description: Returns a list with all the questions
+   *    produces:
+   *      - application/json
+   *    responses:
+   *      200:
+   *        description: Successfully retrives the list of questions
+   *        schema:
+   *          $ref: '#/components/schemas/BasicQuestionModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                questions:
+   *                  type: array
+   *                  items:
+   *                    type: object
+   *                    $ref: '#/components/schemas/BasicQuestionModel'
+   *      500:
+   *        description: The server was unable to get the list of questions
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async list (req, res) {
     const questions = await Question.findAll()
 
@@ -34,6 +146,70 @@ class QuestionController {
     res.json({ questions })
   }
 
+  /**
+   * @swagger
+   * /questions/{id}:
+   *  put:
+   *    tags:
+   *      - Questions
+   *    description: Updates a question instance
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    requestBody:
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              text:
+   *                type: string
+   *    responses:
+   *      200:
+   *        description: Successfully updates a question
+   *        schema:
+   *          $ref: '#/components/schemas/BasicQuestionModel'
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                question:
+   *                  type: object
+   *                  $ref: '#/components/schemas/BasicQuestionModel'
+   *      400:
+   *        description: Attempt to make an empty question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      404:
+   *        description: The server was unable to find the question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      500:
+   *        description: The sever was unable to update the question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async update (req, res) {
     const { id } = req.params
     const { text } = req.body
@@ -55,6 +231,50 @@ class QuestionController {
     res.json({ question: updated })
   }
 
+  /**
+   * @swagger
+   * /questions/{id}:
+   *  delete:
+   *    tags:
+   *      - Questions
+   *    description: Deletes a question instance
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    responses:
+   *      200:
+   *        description: Successfully deletes the question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      404:
+   *        description: The server was unable to find the question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   *      500:
+   *        description: The sever was unable to delete the question
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                message:
+   *                  type: string
+   */
   async delete (req, res) {
     const { id } = req.params
     const deleted = await Question.destroy({ where: { id: id } })
@@ -63,7 +283,7 @@ class QuestionController {
       return res.status(404).json({ message: 'Question not found.' })
     }
 
-    return res.json('Question deleted!')
+    return res.json({ message: 'Question deleted!' })
   }
 }
 module.exports = new QuestionController()

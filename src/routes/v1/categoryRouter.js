0f01b5fd70f04caf9898a837984949df36e9e543
Zambom <erikzambom@hotmail.com>
2019-10-23 13:01:25 -0300

    Applying middlewares to category routes

--- src/routes/v1/categoryRouter.js
+++ src/routes/v1/categoryRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const { CategoryController } = require('../../controllers')
 
+const { authorization, permissions } = require('../../middlewares')
+
 router.get('/', CategoryController.list)
 router.get('/:id', CategoryController.view)
-router.post('/', CategoryController.create)
-router.put('/:id', CategoryController.update)
-router.delete('/:id', CategoryController.delete)
+router.post('/', authorization, permissions, CategoryController.create)
+router.put('/:id', authorization, permissions, CategoryController.update)
+router.delete('/:id', authorization, permissions, CategoryController.delete)
 
 module.exports = router

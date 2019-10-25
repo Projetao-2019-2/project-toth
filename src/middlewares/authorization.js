2da84d5e0074a3438c3fc817344e3efb5674a6d3
Zambom <erikzambom@hotmail.com>
2019-10-25 09:41:46 -0300

    Adding bearer token schema

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -5,12 +5,12 @@ const {
 module.exports = async (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers.authorization
 
-  if (token.startsWith('Bearer ')) {
-    token = token.slice(7, token.length)
-  }
-
   if (!token) {
     return res.status(401).send({ message: 'Token not provided' })
+  }
+
+  if (token.startsWith('Bearer ')) {
+    token = token.slice(7, token.length)
   }
 
   const decoded = await verifyToken(token, 'nortuni')
@@ -22,7 +22,7 @@ module.exports = async (req, res, next) => {
   const { access, expires } = decoded.obj
   const now = new Date()
 
-  if (Math.abs(now.getTime() - access.getTime()) > expires) {
+  if (Math.abs(now.getTime() - new Date(access).getTime()) / 1000 > expires) {
     return res.status(401).send({ message: 'Token expired' })
   }
 
--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -2,6 +2,10 @@ module.exports = (req, res, next) => {
   if (req.user && req.user.type === 'admin') {
     next()
   } else {
-    res.status(403).json({ message: 'Forbidden' })
+    res
+      .status(403)
+      .json({
+        message: 'Only administrators have permission to do this request'
+      })
   }
 }

1f55bf43a2eba09cc06e882561e74346a860acb6
Zambom <erikzambom@hotmail.com>
2019-10-25 09:42:16 -0300

    Applying middlewares to question routes

--- src/routes/v1/questionRouter.js
+++ src/routes/v1/questionRouter.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 
 const { QuestionController } = require('../../controllers')
 
-router.post('/', QuestionController.create)
+const { authorization } = require('../../middlewares')
+
+router.post('/', authorization, QuestionController.create)
 router.get('/', QuestionController.list)
 router.get('/:id', QuestionController.view)
-router.put('/:id', QuestionController.update)
-router.delete('/:id', QuestionController.delete)
+router.put('/:id', authorization, QuestionController.update)
+router.delete('/:id', authorization, QuestionController.delete)
 
 module.exports = router

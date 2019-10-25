3cef956da5c72c3b60ce942c2c327f43d78be6b1
Zambom <erikzambom@hotmail.com>
2019-10-25 09:57:00 -0300

    Adjusting admin permissions verification

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -1,11 +1,9 @@
 module.exports = (req, res, next) => {
-  if (req.user && req.user.type === 'admin') {
+  if (req.user && req.user.tipo === 'admin') {
     next()
   } else {
-    res
-      .status(403)
-      .json({
-        message: 'Only administrators have permission to do this request'
-      })
+    res.status(403).json({
+      message: 'Only administrators have permission to do this request'
+    })
   }
 }

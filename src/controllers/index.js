a574ca3c5b4fd632e8d1f8cc3c7869b84ebe6624
lgsa2 <56773679+lgsa2@users.noreply.github.com>
2019-10-28 14:18:41 -0300

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,12 +4,14 @@ const QuestionController = require(`./${process.env.API_VERSION}/question`)
 const CategoryController = require(`./${process.env.API_VERSION}/category`)
 const CommentController = require(`./${process.env.API_VERSION}/comment`)
 const UserController = require(`./${process.env.API_VERSION}/user`)
+const AchievementController = require(`./${process.env.API_VERSION}/achievement`)
 
 module.exports = {
-  AuthController,
-  PostController,
-  QuestionController,
-  CategoryController,
-  CommentController,
-  UserController
+    AuthController,
+    PostController,
+    QuestionController,
+    CategoryController,
+    CommentController,
+    UserController,
+    AchievementController
 }

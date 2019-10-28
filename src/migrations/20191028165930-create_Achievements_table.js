550ddaaa16f6332d3bd41fa5cdfd1a6765641008
lgsa2 <56773679+lgsa2@users.noreply.github.com>
2019-10-28 14:17:02 -0300

    Update 20191028165930-create_Achievements_table.js

--- src/migrations/20191028165930-create_Achievements_table.js
+++ src/migrations/20191028165930-create_Achievements_table.js
@@ -2,22 +2,36 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      descricao: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      titulo: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      pontos: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      createdat: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedat: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('achievements')
   }
 };
